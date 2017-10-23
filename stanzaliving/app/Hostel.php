2ba44acbf9456db7953f25040f0c4955389f86f0
unknown <gupta.ankur792@gmail.com>
2017-10-23 12:39:54 +0530

    ankur

---
+++ stanzaliving/app/Hostel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hostel extends Model
+{
+
+	protected $fillable=['name','contact','hoteldesc','hoteldesclist','addr','facility','img','googleurl'];
+
+	
+    //
+}
